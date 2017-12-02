15ff32fcabdeb734ea57aeae0d31ab6a26cea335
despacito12 <31024960+despacito12@users.noreply.github.com>
2017-12-02 14:16:57 +0800

    Update sqlite.js

--- www/js/sqlite.js
+++ www/js/sqlite.js
@@ -1,271 +1,173 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var TodoBox = React.createClass({
+  displayName: "TodoBox",
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var RecipeInfo = function (_React$Component) {
-  _inherits(RecipeInfo, _React$Component);
-
-  function RecipeInfo() {
-    _classCallCheck(this, RecipeInfo);
-
-    var _this = _possibleConstructorReturn(this, _React$Component.call(this));
-
-    _this.state = {
-      isExpanded: false
+  getInitialState: function getInitialState() {
+    return {
+      data: [{ "id": "00001", "task": "Buy Dog", "complete": "false" }, { "id": "00002", "task": "Learn how to rap", "complete": "false" }, { "id": "00003", "task": "Learn how to twerk like miley", "complete": "false" }]
     };
-    _this.toggleExpansion = _this.toggleExpansion.bind(_this);
-    _this.delete = _this.delete.bind(_this);
-    _this.editRecipe = _this.editRecipe.bind(_this);
-    return _this;
-  }
-
-  RecipeInfo.prototype.delete = function _delete() {
-    this.props.deleteFn(this.props.index);
-  };
-
-  RecipeInfo.prototype.toggleExpansion = function toggleExpansion() {
-    this.setState({
-      isExpanded: !this.state.isExpanded
+  },
+  generateId: function generateId() {
+    return Math.floor(Math.random() * 90000) + 10000;
+  },
+  handleNodeRemoval: function handleNodeRemoval(nodeId) {
+    var data = this.state.data;
+    data = data.filter(function (el) {
+      return el.id !== nodeId;
     });
-  };
-
-  RecipeInfo.prototype.editRecipe = function editRecipe() {
-    this.props.editRecipe(this.props.index);
-  };
-
-  RecipeInfo.prototype.render = function render() {
-    if (this.state.isExpanded === false) return React.createElement(
-      "div",
-      { className: "recipeTitle", onClick: this.toggleExpansion },
-      this.props.recipeDetails.title
-    );else return React.createElement(
-      "div",
-      { className: "recipeWrapper" },
-      React.createElement(
-        "div",
-        { className: "recipeTitle", onClick: this.toggleExpansion },
-        this.props.recipeDetails.title
-      ),
-      this.props.recipeDetails.ingredients.split(",").map(function (ingred, i) {
-        return React.createElement(
-          "div",
-          { className: "ingredientName" },
-          ingred
-        );
-      }),
-      React.createElement(
-        "div",
-        { className: "buttonContainer" },
-        React.createElement(
-          "div",
-          { className: "button-primary", onClick: this.editRecipe },
-          React.createElement("i", { className: "fa fa-pencil-square-o fa-2x", "aria-hidden": "true" })
-        ),
-        React.createElement(
-          "div",
-          { className: "button-primary deleteButton", onClick: this.delete },
-          React.createElement("i", { className: "fa fa-trash fa-2x", "aria-hidden": "true" })
-        )
-      )
-    );
-  };
-
-  return RecipeInfo;
-}(React.Component);
-
-var RecipesSection = function (_React$Component2) {
-  _inherits(RecipesSection, _React$Component2);
-
-  function RecipesSection() {
-    _classCallCheck(this, RecipesSection);
-
-    var _this2 = _possibleConstructorReturn(this, _React$Component2.call(this));
-
-    _this2.state = {
-      recipes: typeof localStorage["thinkTankRecipes"] != "undefined" ? JSON.parse(localStorage["thinkTankRecipes"]) : [{ title: "Butter chicken", ingredients: "Butter,chicken" }, { title: "Cake", ingredients: "chocolate,MAGIC!" }],
-      recipeIndex: -1,
-      newRecipeTitle: "Enter title here",
-      newRecipeIngredients: "Enter ingredients seperated by commas eg (butter,sugar,chocolate)"
-    };
-    _this2.saveRecipe = _this2.saveRecipe.bind(_this2);
-    _this2.toggleMaker = _this2.toggleMaker.bind(_this2);
-    _this2.sanitizeRecipe = _this2.sanitizeRecipe.bind(_this2);
-    _this2.resetRecipeValues = _this2.resetRecipeValues.bind(_this2);
-    _this2.deleteRecipe = _this2.deleteRecipe.bind(_this2);
-    _this2.titleChanged = _this2.titleChanged.bind(_this2);
-    _this2.recipeChanged = _this2.recipeChanged.bind(_this2);
-    _this2.editRecipe = _this2.editRecipe.bind(_this2);
-    _this2.updateLocalStorage = _this2.updateLocalStorage.bind(_this2);
-
-    return _this2;
-  }
-
-  RecipesSection.prototype.updateLocalStorage = function updateLocalStorage() {
-    localStorage.setItem("thinkTankRecipes", JSON.stringify(this.state.recipes));
-  };
-
-  RecipesSection.prototype.editRecipe = function editRecipe(index) {
-    var _this3 = this;
-
-    var title = this.state.recipes[index].title;
-    var ingred = this.state.recipes[index].ingredients;
-    this.setState({
-      recipeIndex: index,
-      newRecipeIngredients: ingred,
-      newRecipeTitle: title }, function () {
-      _this3.toggleMaker();
-    });
-  };
-
-  RecipesSection.prototype.toggleMaker = function toggleMaker() {
-    this.setState({
-      makingRecipe: !this.state.makingRecipe
-    });
-  };
-
-  RecipesSection.prototype.deleteRecipe = function deleteRecipe(index) {
-    var _this4 = this;
-
-    var newRecipes = this.state.recipes;
-    newRecipes.splice(index, 1);
-    this.setState({
-      recipes: newRecipes
-    }, function () {
-      _this4.updateLocalStorage();
-    });
-  };
-
-  RecipesSection.prototype.sanitizeRecipe = function sanitizeRecipe() {
-    var trimmedIngredients = this.state.newRecipeIngredients.split(",");
-    trimmedIngredients.forEach(function (val, i) {
-      return trimmedIngredients[i] = val.length > 50 ? val.slice(0, 50).trim() : val.trim();
-    });
-    trimmedIngredients = trimmedIngredients.join(",");
-    var trimmedTitle = this.state.newRecipeTitle.trim();
-    return { title: trimmedTitle, ingredients: trimmedIngredients };
-  };
-
-  RecipesSection.prototype.saveRecipe = function saveRecipe() {
-    var _this5 = this;
-
-    var sanitizedRecipe = this.sanitizeRecipe();
-    var existingRecipes = this.state.recipes;
-    if (this.state.recipeIndex === -1 && this.state.newRecipeTitle !== "") existingRecipes.push(sanitizedRecipe);else if (this.state.newRecipeTitle !== "") existingRecipes.splice(this.state.recipeIndex, 1, sanitizedRecipe);
-    this.setState({ recipes: existingRecipes,
-      recipeIndex: -1 }, function () {
-      _this5.resetRecipeValues();
-      _this5.updateLocalStorage();
-    });
-  };
-
-  RecipesSection.prototype.resetRecipeValues = function resetRecipeValues() {
-    var _this6 = this;
-
-    this.setState({
-      newRecipeTitle: "Enter title here",
-      newRecipeIngredients: "Enter ingredients seperated by commas eg (butter,sugar,chocolate)"
-    }, function () {
-      _this6.toggleMaker();
-    });
-  };
-
-  RecipesSection.prototype.titleChanged = function titleChanged(e) {
-    var val = e.target.value;
-    this.setState({
-      newRecipeTitle: val.length > 80 ? val.slice(0, 50) : val
-    });
-  };
-
-  RecipesSection.prototype.recipeChanged = function recipeChanged(e) {
-    var val = e.target.value;
-    this.setState({
-      newRecipeIngredients: val
-    });
-  };
-
-  RecipesSection.prototype.render = function render() {
-    var _this7 = this;
-
-    if (this.state.makingRecipe) return React.createElement(
-      "div",
-      { className: "recipeMaker" },
-      React.createElement(
-        "div",
-        { className: "textAreaWrapper" },
-        React.createElement("textArea", { className: "titleEditor", onChange: this.titleChanged, value: this.state.newRecipeTitle })
-      ),
-      React.createElement(
-        "div",
-        { className: "textAreaWrapper" },
-        React.createElement("textArea", { className: "ingredientEditor", onChange: this.recipeChanged, value: this.state.newRecipeIngredients })
-      ),
-      React.createElement(
-        "div",
-        { className: "buttonContainer" },
-        React.createElement(
-          "div",
-          { className: "button-primary saveButton", onClick: this.saveRecipe },
-          React.createElement("i", { className: "fa fa-floppy-o fa-2x", "aria-hidden": "true" })
-        ),
-        React.createElement(
-          "div",
-          { className: "button-primary backButton", onClick: this.resetRecipeValues },
-          React.createElement("i", { className: "fa fa-arrow-left fa-2x", "aria-hidden": "true" })
-        )
-      )
-    );else return React.createElement(
-      "div",
-      { className: "recipesSection" },
-      React.createElement(
-        "div",
-        { className: "buttonContainer", onClick: this.toggleMaker },
-        React.createElement("i", { className: "button-primary fa fa-plus-square fa-2x", "aria-hidden": "true" })
-      ),
-      this.state.recipes.map(function (recipeDetails, i) {
-        return React.createElement(RecipeInfo, { key: i, recipeDetails: recipeDetails, deleteFn: _this7.deleteRecipe, index: i, editRecipe: _this7.editRecipe });
-      })
-    );
-  };
-
-  return RecipesSection;
-}(React.Component);
-
-var App = function (_React$Component3) {
-  _inherits(App, _React$Component3);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _possibleConstructorReturn(this, _React$Component3.apply(this, arguments));
-  }
-
-  App.prototype.render = function render() {
+    this.setState({ data: data });
+    return;
+  },
+  handleSubmit: function handleSubmit(task) {
+    var data = this.state.data;
+    var id = this.generateId().toString();
+    var complete = 'false';
+    data = data.concat([{ id: id, task: task, complete: complete }]);
+    this.setState({ data: data });
+  },
+  handleToggleComplete: function handleToggleComplete(nodeId) {
+    var data = this.state.data;
+    for (var i in data) {
+      if (data[i].id == nodeId) {
+        data[i].complete = data[i].complete === 'true' ? 'false' : 'true';
+        break;
+      }
+    }
+    this.setState({ data: data });
+    return;
+  },
+  render: function render() {
     return React.createElement(
       "div",
-      { className: "App" },
+      { className: "well" },
       React.createElement(
-        "h1",
-        null,
-        "Recipe Box"
+        "h3",
+        { className: "vert-offset-top-0" },
+        "My Wish list"
       ),
-      React.createElement(RecipesSection, null),
+      React.createElement(TodoList, { data: this.state.data, removeNode: this.handleNodeRemoval, toggleComplete: this.handleToggleComplete }),
+      React.createElement(TodoForm, { onTaskSubmit: this.handleSubmit })
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  displayName: "TodoList",
+
+  removeNode: function removeNode(nodeId) {
+    this.props.removeNode(nodeId);
+    return;
+  },
+  toggleComplete: function toggleComplete(nodeId) {
+    this.props.toggleComplete(nodeId);
+    return;
+  },
+  render: function render() {
+    var listNodes = this.props.data.map(function (listItem) {
+      return React.createElement(TodoItem, { key: listItem.id, nodeId: listItem.id, task: listItem.task, complete: listItem.complete, removeNode: this.removeNode, toggleComplete: this.toggleComplete });
+    }, this);
+    return React.createElement(
+      "ul",
+      { className: "list-group" },
+      listNodes
+    );
+  }
+});
+
+var TodoItem = React.createClass({
+  displayName: "TodoItem",
+
+  removeNode: function removeNode(e) {
+    e.preventDefault();
+    this.props.removeNode(this.props.nodeId);
+    return;
+  },
+  toggleComplete: function toggleComplete(e) {
+    e.preventDefault();
+    this.props.toggleComplete(this.props.nodeId);
+    return;
+  },
+  updateClass: function updateClass() {},
+  render: function render() {
+    var classes = 'list-group-item clearfix';
+    if (this.props.complete === 'true') {
+      classes = classes + ' list-group-item-success';
+    }
+    return React.createElement(
+      "li",
+      { className: classes },
+      this.props.task,
       React.createElement(
         "div",
-        { className: "signature" },
+        { className: "pull-right", role: "group" },
         React.createElement(
-          "p",
-          null,
-          React.createElement("i", { className: "fa fa-heart", "aria-hidden": "true" })
-        )/**/
+          "button",
+          { type: "button", className: "btn btn-xs btn-success img-circle", onClick: this.toggleComplete },
+          "✓"
+        ),
+        " ",
+        React.createElement(
+          "button",
+          { type: "button", className: "btn btn-xs btn-danger img-circle", onClick: this.removeNode },
+          "Ｘ"
+        )
       )
     );
-  };
+  }
+});
 
-  return App;
-}(React.Component);
+var TodoForm = React.createClass({
+  displayName: "TodoForm",
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+  doSubmit: function doSubmit(e) {
+    e.preventDefault();
+    var task = React.findDOMNode(this.refs.task).value.trim();
+    if (!task) {
+      return;
+    }
+    this.props.onTaskSubmit(task);
+    React.findDOMNode(this.refs.task).value = '';
+    return;
+  },
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "commentForm vert-offset-top-2" },
+      React.createElement("hr", null),
+      React.createElement(
+        "div",
+        { className: "clearfix" },
+        React.createElement(
+          "form",
+          { className: "todoForm form-horizontal", onSubmit: this.doSubmit },
+          React.createElement(
+            "div",
+            { className: "form-group" },
+            React.createElement(
+              "label",
+              { htmlFor: "task", className: "col-md-2 control-label" },
+              "Task"
+            ),
+            React.createElement(
+              "div",
+              { className: "col-md-10" },
+              React.createElement("input", { type: "text", id: "task", ref: "task", className: "form-control", placeholder: "What do you need to do?" })
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "row" },
+            React.createElement(
+              "div",
+              { className: "col-md-10 col-md-offset-2 text-right" },
+              React.createElement("input", { type: "submit", value: "Save Item", className: "btn btn-primary" })
+            )
+          )
+        )
+      )
+    );
+  }
+});
+
+React.render(React.createElement(TodoBox, null), document.getElementById('todoBox'));
