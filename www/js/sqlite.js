4bec1f232d9c28d86954acef3a45318e171043d2
despacito12 <31024960+despacito12@users.noreply.github.com>
2017-12-02 09:22:06 +0800

    Update sqlite.js

--- www/js/sqlite.js
+++ www/js/sqlite.js
@@ -6,247 +6,266 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Button = function Button(props) {
-  var classes = props.buttonType + " " + props.buttonDetail;
-  return React.createElement(
-    "button",
-    { value: props.value, onClick: props.onClick, className: classes },
-    props.value
-  );
-};
+var RecipeInfo = function (_React$Component) {
+  _inherits(RecipeInfo, _React$Component);
 
-var ButtonBox = function ButtonBox(props) {
-  return React.createElement(
-    "div",
-    { className: "button-box" },
-    React.createElement(Button, { onClick: props.clearHandler, buttonType: "clear-btn", buttonDetail: "clearall", value: "CE" }),
-    React.createElement(Button, { onClick: props.clearHandler, buttonType: "clear-btn", buttonDetail: "clear", value: "C" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "7" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "8" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "9" }),
-    React.createElement(Button, { onClick: props.operatorHandler, buttonType: "number-btn", buttonDetail: "operator", value: "÷" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "4" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "5" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "6" }),
-    React.createElement(Button, { onClick: props.operatorHandler, buttonType: "number-btn", buttonDetail: "operator", value: "×" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "1" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "2" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "3" }),
-    React.createElement(Button, { onClick: props.operatorHandler, buttonType: "number-btn", buttonDetail: "operator", value: "-" }),
-    React.createElement(Button, { onClick: props.equalHandler, buttonType: "number-btn", buttonDetail: "equal", value: "=" }),
-    React.createElement(Button, { onClick: props.length < 10 ? props.numberHandler : null, buttonType: "number-btn", buttonDetail: "number", value: "0" }),
-    React.createElement(Button, { onClick: props.pointHandler, buttonType: "number-btn", buttonDetail: "point", value: "." }),
-    React.createElement(Button, { onClick: props.operatorHandler, buttonType: "number-btn", buttonDetail: "operator", value: "+" })
-  );
-};
+  function RecipeInfo() {
+    _classCallCheck(this, RecipeInfo);
 
-var Calculator = function (_React$Component) {
-  _inherits(Calculator, _React$Component);
-
-  function Calculator(props) {
-    _classCallCheck(this, Calculator);
-
-    var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
+    var _this = _possibleConstructorReturn(this, _React$Component.call(this));
 
     _this.state = {
-      number: "",
-      sequenceArray: [],
-      currentIndex: 0,
-      oldNumber: "",
-      result: "",
-      operator: "",
-      equalized: false,
-      chainOps: false,
-      pointCheck: false
+      isExpanded: false
     };
+    _this.toggleExpansion = _this.toggleExpansion.bind(_this);
+    _this.delete = _this.delete.bind(_this);
+    _this.editRecipe = _this.editRecipe.bind(_this);
     return _this;
   }
 
-  Calculator.prototype.execute = function execute() {
-    var result = "";
-    switch (this.state.operator) {
-      case "+":
-        result = (parseFloat(this.state.oldNumber) + parseFloat(this.state.number)).toString();
-        break;
-      case "-":
-        result = (parseFloat(this.state.oldNumber) - parseFloat(this.state.number)).toString();
-        break;
-      case "÷":
-        result = (parseFloat(this.state.oldNumber) / parseFloat(this.state.number)).toString();
-        break;
-      case "×":
-        result = (parseFloat(this.state.oldNumber) * parseFloat(this.state.number)).toString();
-        break;
-    }
-    if (result.indexOf(".") && result.length > 10) {
-      var beforePoint = Math.round(parseFloat(result)).toString().length;
-      if (beforePoint < 10) {
-        var zeroes = "1";
-        for (var i = 10 - beforePoint; i > 0; i--) {
-          zeroes += "0";
-        }
-        result = (Math.round(result * zeroes) / zeroes).toString();
-      } else {
-        result = "ERROR";
-      }
-    } else if (result.length > 10) {
-      result = "ERROR";
-    }
-    if (result === "ERROR") {
-      this.setState({
-        pointCheck: false,
-        equalized: true,
-        oldNumber: "",
-        number: "",
-        operator: ""
-      });
-    }
-    return result;
+  RecipeInfo.prototype.delete = function _delete() {
+    this.props.deleteFn(this.props.index);
   };
 
-  Calculator.prototype.clearHandler = function clearHandler(event) {
-    var sequenceArray = this.state.sequenceArray.slice();
-    if (event.target.value !== "CE") {
-      sequenceArray[this.state.currentIndex] = "";
-    }
+  RecipeInfo.prototype.toggleExpansion = function toggleExpansion() {
     this.setState({
-      // numberSequence: event.target.value === "CE" ?
-      //   "" :
-      //   this.state.numberSequence.substr(0, this.state.numberSequence.length - this.state.number.length),
-      sequenceArray: event.target.value !== "CE" ? sequenceArray : [],
-      currentIndex: event.target.value !== "CE" ? this.state.currentIndex : 0,
-      pointCheck: false,
-      number: "",
-      result: "",
-      operator: event.target.value !== "CE" ? this.state.operator : "",
-      equalized: false,
-      chainOps: false,
-      error: false
+      isExpanded: !this.state.isExpanded
     });
   };
 
-  Calculator.prototype.numberHandler = function numberHandler(event) {
-    var currentIndex = this.state.currentIndex !== 0 ? this.state.currentIndex : 0;
-    var number = "" + (this.state.number !== "0" && !this.state.equalized && !this.state.chainOps ? this.state.number : "") + event.target.value;
-    var sequenceArray = !this.state.equalized ? this.state.sequenceArray.slice() : [];
-    sequenceArray[currentIndex] = number;
-    this.setState({
-      result: "",
-      number: number,
-      sequenceArray: sequenceArray,
-      // numberSequence:
-      //   `${
-      //   this.state.numberSequence !== "0" && !this.state.equalized ?
-      //   this.state.numberSequence :
-      //   ""
-      // }${event.target.value}`,
-      equalized: false,
-      chainOps: false,
-      currentIndex: currentIndex
-    });
+  RecipeInfo.prototype.editRecipe = function editRecipe() {
+    this.props.editRecipe(this.props.index);
   };
 
-  Calculator.prototype.operatorHandler = function operatorHandler(event) {
-    var sequenceArray = this.state.sequenceArray.slice();
-    sequenceArray.push(event.target.value);
-    if (this.state.number !== "") {
-      var result = "";
-      if (this.state.operator !== "") {
-        result = this.execute();
-      }
-      this.setState({
-        pointCheck: false,
-        operator: event.target.value,
-        result: result,
-        oldNumber: this.state.operator === "" ? this.state.number : result !== "" ? result : this.state.number,
-        number: this.state.operator === "" ? "" : this.state.number,
-        // numberSequence: `${this.state.numberSequence}${event.target.value}`,
-        chainOps: this.state.operator !== "",
-        currentIndex: this.state.currentIndex += 2,
-        sequenceArray: sequenceArray
-      });
-    }
-  };
-
-  Calculator.prototype.equalHandler = function equalHandler() {
-    if (this.state.number !== "" && this.state.oldNumber !== "" && !isNaN(this.state.sequenceArray[this.state.currentIndex])) {
-      var result = "";
-      result = this.execute();
-      this.setState({
-        result: result,
-        pointCheck: false,
-        equalized: true,
-        oldNumber: "",
-        number: "",
-        operator: ""
-      });
-    }
-  };
-
-  Calculator.prototype.pointHandler = function pointHandler() {
-    var number = this.state.number !== "" ? this.state.number + "." : "0.";
-    var sequenceArray = this.state.sequenceArray.slice();
-    sequenceArray[this.state.currentIndex] = number;
-    if (!this.state.pointCheck) {
-      this.setState({
-        number: number,
-        // numberSequence: this.state.numberSequence !== "" ?
-        //   `${this.state.numberSequence}.` :
-        //   `0.`,
-        pointCheck: true,
-        sequenceArray: sequenceArray
-      });
-    }
-  };
-
-  Calculator.prototype.render = function render() {
-    return React.createElement(
+  RecipeInfo.prototype.render = function render() {
+    if (this.state.isExpanded === false) return React.createElement(
       "div",
-      { className: "container", id: "main-view" },
+      { className: "recipeTitle", onClick: this.toggleExpansion },
+      this.props.recipeDetails.title
+    );else return React.createElement(
+      "div",
+      { className: "recipeWrapper" },
       React.createElement(
         "div",
-        { className: "calculator-ext" },
+        { className: "recipeTitle", onClick: this.toggleExpansion },
+        this.props.recipeDetails.title
+      ),
+      this.props.recipeDetails.ingredients.split(",").map(function (ingred, i) {
+        return React.createElement(
+          "div",
+          { className: "ingredientName" },
+          ingred
+        );
+      }),
+      React.createElement(
+        "div",
+        { className: "buttonContainer" },
         React.createElement(
           "div",
-          { className: "calculator-top-div" },
-          React.createElement(
-            "h4",
-            null,
-            "jessica's REACT Calculator"
-          )
+          { className: "button-primary", onClick: this.editRecipe },
+          React.createElement("i", { className: "fa fa-pencil-square-o fa-2x", "aria-hidden": "true" })
         ),
         React.createElement(
           "div",
-          { className: "calculator-int" },
-          React.createElement(
-            "div",
-            { className: "text-box" },
-            React.createElement(
-              "div",
-              { type: "text", name: "upper-display", className: "display-up" },
-              this.state.equalized || this.state.chainOps ? this.state.result : this.state.number !== "" ? this.state.number : 0
-            ),
-            React.createElement(
-              "div",
-              { type: "text", name: "lower-display", className: "display-down" },
-              /*{this.state.numberSequence !== "" ? this.state.numberSequence : 0}*/
-              this.state.sequenceArray.join("") !== "" ? this.state.sequenceArray.join("") : 0
-            )
-          ),
-          React.createElement(ButtonBox, {
-            numberHandler: this.numberHandler.bind(this),
-            operatorHandler: this.operatorHandler.bind(this),
-            clearHandler: this.clearHandler.bind(this),
-            pointHandler: this.pointHandler.bind(this),
-            equalHandler: this.equalHandler.bind(this),
-            length: this.state.number.length
-          })
+          { className: "button-primary deleteButton", onClick: this.delete },
+          React.createElement("i", { className: "fa fa-trash fa-2x", "aria-hidden": "true" })
         )
       )
     );
   };
 
-  return Calculator;
+  return RecipeInfo;
 }(React.Component);
 
-ReactDOM.render(React.createElement(Calculator, null), document.getElementById('root'));
+var RecipesSection = function (_React$Component2) {
+  _inherits(RecipesSection, _React$Component2);
+
+  function RecipesSection() {
+    _classCallCheck(this, RecipesSection);
+
+    var _this2 = _possibleConstructorReturn(this, _React$Component2.call(this));
+
+    _this2.state = {
+      recipes: typeof localStorage["thinkTankRecipes"] != "undefined" ? JSON.parse(localStorage["thinkTankRecipes"]) : [{ title: "Butter chicken", ingredients: "Butter,chicken" }, { title: "Cake", ingredients: "chocolate,MAGIC!" }],
+      recipeIndex: -1,
+      newRecipeTitle: "Enter title here",
+      newRecipeIngredients: "Enter ingredients seperated by commas eg (butter,sugar,chocolate)"
+    };
+    _this2.saveRecipe = _this2.saveRecipe.bind(_this2);
+    _this2.toggleMaker = _this2.toggleMaker.bind(_this2);
+    _this2.sanitizeRecipe = _this2.sanitizeRecipe.bind(_this2);
+    _this2.resetRecipeValues = _this2.resetRecipeValues.bind(_this2);
+    _this2.deleteRecipe = _this2.deleteRecipe.bind(_this2);
+    _this2.titleChanged = _this2.titleChanged.bind(_this2);
+    _this2.recipeChanged = _this2.recipeChanged.bind(_this2);
+    _this2.editRecipe = _this2.editRecipe.bind(_this2);
+    _this2.updateLocalStorage = _this2.updateLocalStorage.bind(_this2);
+
+    return _this2;
+  }
+
+  RecipesSection.prototype.updateLocalStorage = function updateLocalStorage() {
+    localStorage.setItem("thinkTankRecipes", JSON.stringify(this.state.recipes));
+  };
+
+  RecipesSection.prototype.editRecipe = function editRecipe(index) {
+    var _this3 = this;
+
+    var title = this.state.recipes[index].title;
+    var ingred = this.state.recipes[index].ingredients;
+    this.setState({
+      recipeIndex: index,
+      newRecipeIngredients: ingred,
+      newRecipeTitle: title }, function () {
+      _this3.toggleMaker();
+    });
+  };
+
+  RecipesSection.prototype.toggleMaker = function toggleMaker() {
+    this.setState({
+      makingRecipe: !this.state.makingRecipe
+    });
+  };
+
+  RecipesSection.prototype.deleteRecipe = function deleteRecipe(index) {
+    var _this4 = this;
+
+    var newRecipes = this.state.recipes;
+    newRecipes.splice(index, 1);
+    this.setState({
+      recipes: newRecipes
+    }, function () {
+      _this4.updateLocalStorage();
+    });
+  };
+
+  RecipesSection.prototype.sanitizeRecipe = function sanitizeRecipe() {
+    var trimmedIngredients = this.state.newRecipeIngredients.split(",");
+    trimmedIngredients.forEach(function (val, i) {
+      return trimmedIngredients[i] = val.length > 50 ? val.slice(0, 50).trim() : val.trim();
+    });
+    trimmedIngredients = trimmedIngredients.join(",");
+    var trimmedTitle = this.state.newRecipeTitle.trim();
+    return { title: trimmedTitle, ingredients: trimmedIngredients };
+  };
+
+  RecipesSection.prototype.saveRecipe = function saveRecipe() {
+    var _this5 = this;
+
+    var sanitizedRecipe = this.sanitizeRecipe();
+    var existingRecipes = this.state.recipes;
+    if (this.state.recipeIndex === -1 && this.state.newRecipeTitle !== "") existingRecipes.push(sanitizedRecipe);else if (this.state.newRecipeTitle !== "") existingRecipes.splice(this.state.recipeIndex, 1, sanitizedRecipe);
+    this.setState({ recipes: existingRecipes,
+      recipeIndex: -1 }, function () {
+      _this5.resetRecipeValues();
+      _this5.updateLocalStorage();
+    });
+  };
+
+  RecipesSection.prototype.resetRecipeValues = function resetRecipeValues() {
+    var _this6 = this;
+
+    this.setState({
+      newRecipeTitle: "Enter title here",
+      newRecipeIngredients: "Enter ingredients seperated by commas eg (butter,sugar,chocolate)"
+    }, function () {
+      _this6.toggleMaker();
+    });
+  };
+
+  RecipesSection.prototype.titleChanged = function titleChanged(e) {
+    var val = e.target.value;
+    this.setState({
+      newRecipeTitle: val.length > 80 ? val.slice(0, 50) : val
+    });
+  };
+
+  RecipesSection.prototype.recipeChanged = function recipeChanged(e) {
+    var val = e.target.value;
+    this.setState({
+      newRecipeIngredients: val
+    });
+  };
+
+  RecipesSection.prototype.render = function render() {
+    var _this7 = this;
+
+    if (this.state.makingRecipe) return React.createElement(
+      "div",
+      { className: "recipeMaker" },
+      React.createElement(
+        "div",
+        { className: "textAreaWrapper" },
+        React.createElement("textArea", { className: "titleEditor", onChange: this.titleChanged, value: this.state.newRecipeTitle })
+      ),
+      React.createElement(
+        "div",
+        { className: "textAreaWrapper" },
+        React.createElement("textArea", { className: "ingredientEditor", onChange: this.recipeChanged, value: this.state.newRecipeIngredients })
+      ),
+      React.createElement(
+        "div",
+        { className: "buttonContainer" },
+        React.createElement(
+          "div",
+          { className: "button-primary saveButton", onClick: this.saveRecipe },
+          React.createElement("i", { className: "fa fa-floppy-o fa-2x", "aria-hidden": "true" })
+        ),
+        React.createElement(
+          "div",
+          { className: "button-primary backButton", onClick: this.resetRecipeValues },
+          React.createElement("i", { className: "fa fa-arrow-left fa-2x", "aria-hidden": "true" })
+        )
+      )
+    );else return React.createElement(
+      "div",
+      { className: "recipesSection" },
+      React.createElement(
+        "div",
+        { className: "buttonContainer", onClick: this.toggleMaker },
+        React.createElement("i", { className: "button-primary fa fa-plus-square fa-2x", "aria-hidden": "true" })
+      ),
+      this.state.recipes.map(function (recipeDetails, i) {
+        return React.createElement(RecipeInfo, { key: i, recipeDetails: recipeDetails, deleteFn: _this7.deleteRecipe, index: i, editRecipe: _this7.editRecipe });
+      })
+    );
+  };
+
+  return RecipesSection;
+}(React.Component);
+
+var App = function (_React$Component3) {
+  _inherits(App, _React$Component3);
+
+  function App() {
+    _classCallCheck(this, App);
+
+    return _possibleConstructorReturn(this, _React$Component3.apply(this, arguments));
+  }
+
+  App.prototype.render = function render() {
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement(
+        "h1",
+        null,
+        "Recipe Box"
+      ),
+      React.createElement(RecipesSection, null),
+      React.createElement(
+        "div",
+        { className: "signature" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement("i", { className: "fa fa-heart", "aria-hidden": "true" })
+        )/**/
+      )
+    );
+  };
+
+  return App;
+}(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
