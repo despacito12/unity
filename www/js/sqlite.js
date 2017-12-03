4fb4f58a2d27e3395aaa2974104ea03d0241a986
despacito12 <31024960+despacito12@users.noreply.github.com>
2017-12-03 19:50:41 +0800

    Update sqlite.js

--- www/js/sqlite.js
+++ www/js/sqlite.js
@@ -10,7 +10,57 @@ var Note = function (_React$Component) {
   _inherits(Note, _React$Component);
 
   function Note() {
-    asa ka r" },
+    _classCallCheck(this, Note);
+
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+  }
+
+  Note.prototype.render = function render() {
+    var style = {
+      backgroundColor: this.props.color
+    };
+    return React.createElement(
+      "div",
+      { className: "note", style: style },
+      this.props.children,
+      React.createElement(
+        "span",
+        { className: "note-delete", onClick: this.props.onDelete },
+        "x"
+      )
+    );
+  };
+
+  return Note;
+}(React.Component);
+
+var NoteEditor = function (_React$Component2) {
+  _inherits(NoteEditor, _React$Component2);
+
+  function NoteEditor(props) {
+    _classCallCheck(this, NoteEditor);
+
+    var _this2 = _possibleConstructorReturn(this, _React$Component2.call(this, props));
+
+    _this2.color = 'ff6680';
+    _this2.state = { text: '', color: _this2.color };
+    return _this2;
+  }
+
+  NoteEditor.prototype.handleChange = function handleChange(e) {
+    this.setState({ text: e.target.value });
+  };
+
+  NoteEditor.prototype.handleChangeColor = function handleChangeColor(e) {
+    this.setState({ color: e.target.value });
+  };
+
+  NoteEditor.prototype.render = function render() {
+    var _this3 = this;
+
+    return React.createElement(
+      "div",
+      { className: "note-editor" },
       React.createElement(
         "textarea",
         { placeholder: "Enter your note here...", onChange: this.handleChange.bind(this) },
@@ -85,7 +135,7 @@ var NotesApp = function (_React$Component4) {
 
     var _this5 = _possibleConstructorReturn(this, _React$Component4.call(this, props));
 
-    _this5.state = { notes: [{ id: 1, text: 'DemoNote', color: 'coral' }, { id: 2, text: 'You can use Masonry with vanilla JS', color: 'aliceblue' }, { id: 3, text: 'You can initialize Masonry in HTML, without writing any JavaScript', color: 'gold' }, { id: 4, text: 'HTML initialization was previously done with a class of js-masonry and setting options in data-masonry-options in Masonry v3. Masonry v4 is backwards compatible with this code.', color: 'lightpink' }, { id: 5, text: 'Masonry v4 is backwards compatible with this code.', color: '#C9A39C' }] };
+    _this5.state = { notes: [{ id: 1, text: 'DemoNote', color: 'coral' }, { id: 2, text: 'Meet sir Rufo', color: 'aliceblue' }, { id: 3, text: 'Go to the gym', color: 'gold' }, { id: 4, text: 'Try to find Nemo', color: 'lightpink' }, { id: 5, text: 'Eat Big Saging', color: '#C9A39C' }] };
     return _this5;
   }
 
