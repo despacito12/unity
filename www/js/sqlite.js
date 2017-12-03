1cec4b31fa6a28026822d197eb6737cc9fa3873d
despacito12 <31024960+despacito12@users.noreply.github.com>
2017-12-03 14:22:39 +0800

    Update sqlite.js

--- www/js/sqlite.js
+++ www/js/sqlite.js
@@ -1,173 +1,155 @@
 "use strict";
+var storage = window.localStorage;
+var serverData = [
+  {
+  "todo": "Pick up milk from store"
+  }, {
+  "todo": "Make Dr. Appointment"
+  }, {
+  "todo": "Pay Water Bill"
+  }
+];
+var todoList = serverData.map(function (item) {
+  console.log(serverData)
+  console.log(item)
+  return item.todo;
+});
 
-var TodoBox = React.createClass({
-  displayName: "TodoBox",
+var Container = React.createClass({
+  displayName: "Container",
 
   getInitialState: function getInitialState() {
     return {
-      data: [{ "id": "00001", "task": "Buy Dog", "complete": "false" }, { "id": "00002", "task": "Learn how to rap", "complete": "false" }, { "id": "00003", "task": "Learn how to twerk like miley", "complete": "false" }]
+      list: this.props.toDoList
     };
   },
-  generateId: function generateId() {
-    return Math.floor(Math.random() * 90000) + 10000;
+
+  addToDoToList: function addToDoToList(newToDo) {
+    var newList = this.state.list;
+    newList.push(newToDo);
+    this.listUpdate(newList);
   },
-  handleNodeRemoval: function handleNodeRemoval(nodeId) {
-    var data = this.state.data;
-    data = data.filter(function (el) {
-      return el.id !== nodeId;
+
+  removeToDo: function removeToDo(toDo) {
+    var newList = this.state.list;
+    newList = newList.filter(function (item) {
+      return item !== toDo;
     });
-    this.setState({ data: data });
-    return;
+    this.listUpdate(newList);
   },
-  handleSubmit: function handleSubmit(task) {
-    var data = this.state.data;
-    var id = this.generateId().toString();
-    var complete = 'false';
-    data = data.concat([{ id: id, task: task, complete: complete }]);
-    this.setState({ data: data });
+
+  listUpdate: function listUpdate(newList) {
+    //console.log('updating list', newList);
+    storage.setItem("todoList", JSON.stringify(newList));
+    this.setState({
+      list: newList
+    });
   },
-  handleToggleComplete: function handleToggleComplete(nodeId) {
-    var data = this.state.data;
-    for (var i in data) {
-      if (data[i].id == nodeId) {
-        data[i].complete = data[i].complete === 'true' ? 'false' : 'true';
-        break;
-      }
-    }
-    this.setState({ data: data });
-    return;
-  },
+
   render: function render() {
+    var count = this.state.list.length;
     return React.createElement(
       "div",
-      { className: "well" },
-      React.createElement(
-        "h3",
-        { className: "vert-offset-top-0" },
-        "My Wish list"
-      ),
-      React.createElement(TodoList, { data: this.state.data, removeNode: this.handleNodeRemoval, toggleComplete: this.handleToggleComplete }),
-      React.createElement(TodoForm, { onTaskSubmit: this.handleSubmit })
-    );
-  }
-});
-
-var TodoList = React.createClass({
-  displayName: "TodoList",
-
-  removeNode: function removeNode(nodeId) {
-    this.props.removeNode(nodeId);
-    return;
-  },
-  toggleComplete: function toggleComplete(nodeId) {
-    this.props.toggleComplete(nodeId);
-    return;
-  },
-  render: function render() {
-    var listNodes = this.props.data.map(function (listItem) {
-      return React.createElement(TodoItem, { key: listItem.id, nodeId: listItem.id, task: listItem.task, complete: listItem.complete, removeNode: this.removeNode, toggleComplete: this.toggleComplete });
-    }, this);
-    return React.createElement(
-      "ul",
-      { className: "list-group" },
-      listNodes
-    );
-  }
-});
-
-var TodoItem = React.createClass({
-  displayName: "TodoItem",
-
-  removeNode: function removeNode(e) {
-    e.preventDefault();
-    this.props.removeNode(this.props.nodeId);
-    return;
-  },
-  toggleComplete: function toggleComplete(e) {
-    e.preventDefault();
-    this.props.toggleComplete(this.props.nodeId);
-    return;
-  },
-  updateClass: function updateClass() {},
-  render: function render() {
-    var classes = 'list-group-item clearfix';
-    if (this.props.complete === 'true') {
-      classes = classes + ' list-group-item-success';
-    }
-    return React.createElement(
-      "li",
-      { className: classes },
-      this.props.task,
+      { className: "main-container" },
       React.createElement(
         "div",
-        { className: "pull-right", role: "group" },
-        React.createElement(
-          "button",
-          { type: "button", className: "btn btn-xs btn-success img-circle", onClick: this.toggleComplete },
-          "✓"
-        ),
-        " ",
-        React.createElement(
-          "button",
-          { type: "button", className: "btn btn-xs btn-danger img-circle", onClick: this.removeNode },
-          "Ｘ"
-        )
-      )
+        { className: "header-div" },
+        "Dailiry App"
+      ),
+      React.createElement(AddToDoBox, { addItemToList: this.addToDoToList }),
+      React.createElement(ToDoItems, { toDoList: this.state.list, removeToDo: this.removeToDo }),
+      React.createElement(ToDoCount, { count: count })
     );
   }
 });
 
-var TodoForm = React.createClass({
-  displayName: "TodoForm",
+var AddToDoBox = React.createClass({
+  displayName: "AddTextBox",
 
-  doSubmit: function doSubmit(e) {
+  addToDo: function addToDo(e) {
     e.preventDefault();
-    var task = React.findDOMNode(this.refs.task).value.trim();
-    if (!task) {
+    var newToDo = this.refs.AddedItem.value;
+    if (newToDo === "") {
       return;
     }
-    this.props.onTaskSubmit(task);
-    React.findDOMNode(this.refs.task).value = '';
-    return;
+    this.refs.AddedItem.value = "";
+    this.props.addItemToList(newToDo);
   },
   render: function render() {
     return React.createElement(
       "div",
-      { className: "commentForm vert-offset-top-2" },
-      React.createElement("hr", null),
+      { className: "input-div" },
+      React.createElement(
+        "form",
+        null,
+        React.createElement("input", { type: "text", placeholder: " Start typing...", ref: "AddedItem", id: "add-item-input" }),
+        React.createElement("input", { type: "submit", id: "submitToDo", value: "Add", onClick: this.addToDo, id: "add-item-submit" })
+      )
+    );
+  }
+});
+
+var ToDoItems = React.createClass({
+  displayName: "DailyItems",
+
+  removeItem: function remove(e) {
+    var toDo = e.target.dataset.item;
+    this.props.removeToDo(toDo);
+  },
+
+  render: function render() {
+    var items = this.props.toDoList.map(function (item) {
+      return React.createElement(
+        "div",
+        { className: "todo-items" },
+        React.createElement(
+          "span",
+          { className: "item-text" },
+          item
+        ),
+        React.createElement("div", { className: "item-clickable", onDoubleClick: this.removeItem, "data-item": item })
+      );
+    }.bind(this));
+    return React.createElement(
+      "div",
+      null,
+      items
+    );
+  }
+});
+
+var ToDoCount = React.createClass({
+  displayName: "ItemsCount",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "count-div" },
+      React.createElement(
+        "span",
+        null,
+        "Daily Items Left : "
+      ),
       React.createElement(
         "div",
-        { className: "clearfix" },
+        { className: "count-display" },
         React.createElement(
-          "form",
-          { className: "todoForm form-horizontal", onSubmit: this.doSubmit },
-          React.createElement(
-            "div",
-            { className: "form-group" },
-            React.createElement(
-              "label",
-              { htmlFor: "task", className: "col-md-2 control-label" },
-              "Task"
-            ),
-            React.createElement(
-              "div",
-              { className: "col-md-10" },
-              React.createElement("input", { type: "text", id: "task", ref: "task", className: "form-control", placeholder: "What do you need to do?" })
-            )
-          ),
-          React.createElement(
-            "div",
-            { className: "row" },
-            React.createElement(
-              "div",
-              { className: "col-md-10 col-md-offset-2 text-right" },
-              React.createElement("input", { type: "submit", value: "Save Item", className: "btn btn-primary" })
-            )
-          )
+          "span",
+          { className: "count-text" },
+          this.props.count
         )
       )
     );
   }
 });
 
-React.render(React.createElement(TodoBox, null), document.getElementById('todoBox'));
+
+
+if(storage.getItem("todoList") === null){
+  storage.setItem("todoList", JSON.stringify(todoList));
+} else {
+  todoList = JSON.parse(storage.getItem("todoList"));
+};
+
+
+ReactDOM.render(React.createElement(Container, { toDoList: todoList }), document.getElementById("root"));
